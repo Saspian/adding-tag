913c68dc1d60049fda1f60d4217503fe57bc62e8
Saspian <nesaspian@gmail.com>
2019-12-29 17:00:29 +0545

    Updates

---
+++ precache-manifest.40e9b8dbcd55ff74757d9def5847c756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d39eb3eac38d98c19e0be69bef6961",
+    "url": "/tag-module/index.html"
+  },
+  {
+    "revision": "e9c41fe567ada46dfa58",
+    "url": "/tag-module/static/css/main.7292156c.chunk.css"
+  },
+  {
+    "revision": "058c75c9f30dae2f406b",
+    "url": "/tag-module/static/js/2.9258da7e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tag-module/static/js/2.9258da7e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9c41fe567ada46dfa58",
+    "url": "/tag-module/static/js/main.c948c592.chunk.js"
+  },
+  {
+    "revision": "f80d451f0b98259369cc",
+    "url": "/tag-module/static/js/runtime-main.2739bd76.js"
+  }
+]);
